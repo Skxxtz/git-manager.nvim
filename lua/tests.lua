a4e2c7abae95c51eb86e4fc87ae115b8268b1b0d
Sebastian Kootz <skxxtz@gmail.com>
2025-03-25 03:51:16 +0100

    impl: added new trim function test

--- lua/tests.lua
+++ lua/tests.lua
@@ -90,6 +90,7 @@ end
 
 
 local function test_advanced_branches()
+    print("ADVANCED BRANCH REGEX TEST:")
     local lines = {
         "  feature/caching origin/feature/caching",
         "  main origin/main",
@@ -99,12 +100,50 @@ local function test_advanced_branches()
         "* unstable/release-v0.1.5 origin/unstable/release-v0.1.5",
         "* main",
     }
-    for _, line in ipairs(lines) do
-        local active, name, upstream  = line:match("(%**)%s*(%S+)%s*(%S*)%c?$")
-        print(active, name, upstream)
+    local should_be = {
+        {"", "feature/caching", "origin/feature/caching"},
+        {"", "main", "origin/main"},
+        {"", "stable/release-v0.1.2", "origin/stable/release-v0.1.2"},
+        {"", "stable/release-v0.1.3", "origin/stable/release-v0.1.3"},
+        {"", "stable/release-v0.1.4", "origin/stable/release-v0.1.4"},
+        {"*", "unstable/release-v0.1.5", "origin/unstable/release-v0.1.5"},
+        {"*", "main", ""},
+    }
+    for i, line in ipairs(lines) do
+        local active, name, upstream  = line:match("(%**)%s+(%S+)%s*(%S*)%c?$")
+        assert(active == should_be[i][1] and name == should_be[i][2] and upstream == should_be[i][3])
+        print("\27[32mPassed: \27[0m" .. name)
+    end
+    print("\n\n")
+end
+local function trim()
+    print("TRIM TEST")
+    local lines = {
+        "  feature/caching origin/feature/caching",
+        "  main origin/main",
+        "  stable/release-v0.1.2 origin/stable/release-v0.1.2",
+        "  stable/release-v0.1.3 origin/stable/release-v0.1.3",
+        "  stable/release-v0.1.4 origin/stable/release-v0.1.4",
+        "* unstable/release-v0.1.5 origin/unstable/release-v0.1.5",
+        "* main\n ",
+    }
+    local should_be = {
+        "feature/caching origin/feature/caching",
+        "main origin/main",
+        "stable/release-v0.1.2 origin/stable/release-v0.1.2",
+        "stable/release-v0.1.3 origin/stable/release-v0.1.3",
+        "stable/release-v0.1.4 origin/stable/release-v0.1.4",
+        "* unstable/release-v0.1.5 origin/unstable/release-v0.1.5",
+        "* main",
+    }
+    for i, line in ipairs(lines) do
+        local match  = line:match("^%s*(.-)%s*$")
+        assert(match == should_be[i])
+        print("\27[32mPassed: \27[0m" .. match)
     end
 end
 status_note_regex()
 file_regex()
 branch_regex()
 test_advanced_branches()
+trim()
